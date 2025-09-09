12970fb9c16bd96fa06023b9ee0663c90e5de02a
Uswa-Abbas <uswaabbas63@gmail.com>
2025-09-09 02:13:04 -0700

    Add files via upload

---
+++ index.cpp
@@ -0,0 +1,33 @@
+#include "customer.h"
+#include"account.h"
+#include <iostream>
+using namespace std;
+
+int main() {
+    srand(time(0));
+
+    // Option 1: Let user enter info interactively
+    Customer c;
+    c.setCustomerInfo();
+
+    cout << "\nCustomer info you entered:\n";
+    c.getCustomerInfo();
+
+    // Option 2: Search customer by CNIC
+    string searchCNIC;
+    cout << "\nEnter CNIC to search: ";
+    getline(cin, searchCNIC);
+
+    c.getCustomerByCNIC(searchCNIC);
+
+
+    Account acc;
+    acc.openAccount();
+    
+
+
+
+
+
+    return 0;
+}
